dd28efd41f23e55df0835b46018a66641f476025
Colin Kiama <colinkiama@gmail.com>
2022-11-25 23:12:33 +0000

    Render player and obstacle

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,28 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 export default class Demo extends Phaser.Scene {
   constructor() {
-    super('GameScene');
+    super("GameScene");
   }
 
-  preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
-  }
+  preload() {}
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
+    // In Phaser objects are position at their center-points (0,0 is at center of object)
+    const player = this.add.rectangle(
+      25,
+      this.renderer.height - 50 / 2,
+      50,
+      50,
+      0xff0000
+    );
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
+    const obstacle = this.add.rectangle(
+      400,
+      this.renderer.height - 100 / 2,
+      200,
+      100,
+      0x00ff00
+    );
   }
 }
