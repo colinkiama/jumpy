8a04b9fdfd04305f06809d85a3a70f90821e10a5
Colin Kiama <colinkiama@gmail.com>
2022-11-27 18:13:16 +0000

    Obstacle moves towards player

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,19 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 export default {
   type: Phaser.AUTO,
-  parent: 'game',
-  backgroundColor: '#33A5E7',
+  parent: "game",
+  backgroundColor: "#33A5E7",
   scale: {
     width: 800,
     height: 600,
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
 };
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,7 +1,16 @@
 import Phaser from "phaser";
 import Player from "../objects/Player";
 import Obstacle from "../objects/Obstacle";
+
 export default class Demo extends Phaser.Scene {
+  sceneGlobals: {
+    currentObstacle: Obstacle | undefined;
+    currentPlayer: Player | undefined;
+  } = {
+    currentObstacle: undefined,
+    currentPlayer: undefined,
+  };
+
   constructor() {
     super("GameScene");
   }
@@ -11,5 +20,29 @@ export default class Demo extends Phaser.Scene {
   create() {
     const player = new Player(this);
     const obstacle = new Obstacle(this);
+    this.physics.add.existing(player.sprite);
+    this.physics.add.existing(obstacle.sprite);
+    obstacle.sprite.body.velocity.x = -100;
+    this.sceneGlobals.currentObstacle = obstacle;
+    this.sceneGlobals.currentPlayer = player;
+  }
+
+  update() {
+    let massObjects = [
+      this.sceneGlobals.currentObstacle,
+      this.sceneGlobals.currentPlayer,
+    ];
+
+    massObjects.forEach((obj) => {
+      if (obj === undefined) {
+        return;
+      }
+
+      let obstacleBody = obj.sprite.body;
+
+      if (obstacleBody.position.y >= this.renderer.height - obj.height) {
+        obstacleBody.position.y = this.renderer.height - obj.height;
+      }
+    });
   }
 }
