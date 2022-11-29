af34cbe0dccc3561e88ac2edda349cf0dec79ad3
Colin Kiama <colinkiama@gmail.com>
2022-11-29 17:55:32 +0000

    Obstacle overlaps with checkpoint line

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,12 +1,14 @@
-import Phaser from "phaser";
+import Phaser, { Tilemaps } from "phaser";
 import Player from "../objects/Player";
 import Obstacle from "../objects/Obstacle";
 
 export default class Demo extends Phaser.Scene {
   private hasObstacleCollidedWithPlayer: boolean = false;
+  private score: number = 0;
   private keySpace!: Phaser.Input.Keyboard.Key;
   private player!: Player;
   private obstacle!: Obstacle;
+  private checkpointLine!: Phaser.GameObjects.Line;
 
   constructor() {
     super("GameScene");
@@ -24,6 +26,37 @@ export default class Demo extends Phaser.Scene {
     this.keySpace = this.input.keyboard.addKey(
       Phaser.Input.Keyboard.KeyCodes.SPACE
     );
+
+    let thresholdDistance = 10;
+    let checkpointX =
+      this.player.width - thresholdDistance - this.obstacle.width;
+
+    // let checkpointX = 400;
+    this.checkpointLine = this.add.line(
+      checkpointX,
+      this.renderer.height / 2,
+      checkpointX,
+      0,
+      checkpointX,
+      this.renderer.height,
+      0xffff00
+    );
+
+    this.physics.add.existing(this.checkpointLine);
+
+    this.physics.add.overlap(
+      this.checkpointLine,
+      this.obstacle.sprite,
+      this.obstacleReachedCheckpoint.bind(this)
+    );
+
+    console.log("Starting score:", this.score);
+  }
+  obstacleReachedCheckpoint(
+    checkpointLine: Phaser.Types.Physics.Arcade.GameObjectWithBody,
+    sprite: Phaser.Types.Physics.Arcade.GameObjectWithBody
+  ) {
+    this.increaseScore();
   }
 
   update() {
@@ -37,6 +70,14 @@ export default class Demo extends Phaser.Scene {
         objectBody.position.y = this.renderer.height - obj.height;
       }
     });
+
+    if (
+      this.checkpointLine.body.position.y >
+      this.renderer.height - this.checkpointLine.height
+    ) {
+      this.checkpointLine.body.position.y =
+        this.renderer.height - this.checkpointLine.height;
+    }
 
     this.physics.collide(
       this.obstacle.sprite,
@@ -56,6 +97,19 @@ export default class Demo extends Phaser.Scene {
     if (!this.hasObstacleCollidedWithPlayer) {
       this.hasObstacleCollidedWithPlayer = true;
       console.log("Obstacle hit detected");
+
+      // Stop obstacle and player moving after collision
+      obstacle.body.velocity.x = 0;
+      player.body.velocity.x = 0;
     }
+  }
+
+  increaseScore() {
+    this.setScore(this.score + 1);
+  }
+
+  setScore(num: number) {
+    this.score = num;
+    console.log("New Score:", this.score);
   }
 }
