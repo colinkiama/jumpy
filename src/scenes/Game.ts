54326aaff188404aee6c16172e1bc7d454977270
Colin Kiama <colinkiama@gmail.com>
2022-12-03 19:43:40 +0000

    Fix undefined velocity bug

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -167,8 +167,8 @@ export default class Demo extends Phaser.Scene {
   }
 
   setupObstacleCollisions(obstacle: Obstacle) {
-    obstacle.sprite.body.velocity.x = -400;
     this.physics.add.existing(obstacle.sprite);
+    obstacle.sprite.body.velocity.x = -400;
 
     this.physics.add.collider(
       obstacle.sprite,
